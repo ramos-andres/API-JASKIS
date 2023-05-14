589167ae250ac7e8c498f574979f804f3d520c70
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-13 21:18:24 -0700

    "Completed queries, updates and deletions"

--- commands.js
+++ commands.js
@@ -87,18 +87,33 @@ db.bounties.insertMany(
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
+db.bounties.find({ "location": "Grasslands" })
 
 // 2. Query for all bounties with a reward worth 10000 or more
+db.bounties.find({ reward: { $gte: 10000 } })
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
+db.bounties.find({}, {client: 0 })
 
 // 4. Query for a Groundhog in the Woodlands
+db.bounties.find({
+    species: "Groundhog",
+    location: "Woodlands"
+  })
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
+db.bounties.updateOne(
+    { reward: 4000 },
+    { $set: { reward: 10000 } })
 
 // 2. Remove Lokinkajou
+db.bounties.deleteOne({ name: "Lokinkajou" })
 
 // 3. Delete all bounties sent by Songbird
+db.bounties.deleteMany({ client: "Songbird" })
 
 // 4. Update all captured statuses to true
+db.bounties.updateMany(
+    { captured: false },
+    { $set: { captured: true } })
